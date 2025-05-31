13fd58a1d7a62523b91550866ee67eb95f8842aa
Drake364 <143477634+Drake364@users.noreply.github.com>
2025-05-30 21:24:45 -0400

    ddd

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-
 // firebase.js
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
@@ -7,7 +6,28 @@ const firebaseConfig = {
   apiKey: "AIzaSyCsecNhGJQ_nuEIP3bcpIcVgo8TGygKWvM",
   authDomain: "lavajato1-f8fa4.firebaseapp.com",
   projectId: "lavajato1-f8fa4",
-  storageBucket: "lavajato1-f8fa4.firebasestorage.app",
+  storageBucket: Seu arquivo firebase.js está quase correto, mas há um pequeno erro no campo `storageBucket` do objeto `firebaseConfig`. O valor correto deve terminar com `.firebaseapp.com` e não `.firebasestorage.app`.
+
+**Correção sugerida:**
+
+````javascript
+// ...código existente...
+const firebaseConfig = {
+  apiKey: "AIzaSyCsecNhGJQ_nuEIP3bcpIcVgo8TGygKWvM",
+  authDomain: "lavajato1-f8fa4.firebaseapp.com",
+  projectId: "lavajato1-f8fa4",
+  storageBucket: "lavajato1-f8fa4.appspot.com", // corrigido aqui
+  messagingSenderId: "479444330634",
+  appId: "1:479444330634:web:0debaa2d973f999edee573",
+  measurementId: "G-JNLL49FBLL"
+};
+// ...código existente...
+````
+
+**Resumo:**
+- Corrija o campo `storageBucket` para `"lavajato1-f8fa4.appspot.com"`.
+
+Se precisar de mais alguma verificação ou integração, só avisar!, // corrigido aqui
   messagingSenderId: "479444330634",
   appId: "1:479444330634:web:0debaa2d973f999edee573",
   measurementId: "G-JNLL49FBLL"
